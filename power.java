acb4b0f7002ee5e4c54b61a35e4780476cf2898a
JAYASREE1530 <jayasreepaluru1329@gmail.com>
2025-06-02 22:26:44 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+ import java.lang.Math;
+ public class power{
+     public static void main(String[] args)
+     {
+         Scanner sc=new Scanner(System.in);
+         int a=sc.nextInt();
+         double b=Math.pow(a,3);
+         System.out.println(b);
+     }
+ }
+             
+  
+           
