f1b5683b0eff9de87ee28aad4512a661ee5a17f9
JAYASREE1530 <jayasreepaluru1329@gmail.com>
2025-06-03 14:01:11 +0530

    Add files via upload

---
+++ DIGITS.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class DIGIT{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        for(int i=0;i<a.length();i++){
+            char ch=a.charAt(i);
+            if(Character.isDigit(ch)==false)
+            {
+                System.out.println("no digits");
+                System.exit(0);
+            }
+            
+        }
+                    System.out.println("digits");
+                }
+            }
+        
+       
