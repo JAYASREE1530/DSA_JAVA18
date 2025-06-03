7c7136d942ac53183d8d497943b9944381eea439
JAYASREE1530 <jayasreepaluru1329@gmail.com>
2025-06-03 15:06:59 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class year{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%4==0){
+            if(n%100==0){
+             if(n%400==0){
+                 System.out.println("leap year");
+            
+        }
+        else{
+             System.out.println("not leap year");
+        }
+    }else{System.out.println("leap year");
+        
+    }
+}else{System.out.println("not leap year");
+    
+}
+}
+}
